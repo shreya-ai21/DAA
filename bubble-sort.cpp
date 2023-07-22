20605f2ea47110acd5dd63b42b538be7fce8d9e6
shreya-ai21 <shreya.ai21@bmsce.ac.in>
2023-07-22 19:56:53 +0530

    Updated code to take user input

--- bubble-sort.cpp
+++ bubble-sort.cpp
@@ -1,58 +1,44 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
-C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
-// Optimized implementation of Bubble sort
-#include <iostream.h>
+#include <iostream>
+#include <windows.h>
+#include <time.h>
+#include <iomanip>
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 
-// An optimized version of Bubble Sort
-void bubbleSort(int arr[], int n)
-{
-	int i, j;
-	int temp;
-	for (i = 0; i < n - 1; i++) {
-		for (j = 0; j < n - i - 1; j++) {
-			if (arr[j] > arr[j + 1]) {
-				
-				swapped = true;
-			}
-		}
-
-		// If no two elements were swapped
-		// by inner loop, then break
-		if (swapped == false)
-			break;
-	}
-}
-
-// Function to print an array
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << " " << arr[i];
-}
-
-// Driver program to test above functions
 int main()
 {
-    int n;
-    cout<<"Enter number of elements:";
-    cin>>n;
+	int n, key, res;
+	cout << "Enter number of elements:";
+	cin >> n;
 	int arr[n];
-	bubbleSort(arr, n);
-	cout<<"Enter elements:";
-	for(int i=0;i<n;i++)
+	cout << "Enter the elements:";
+	for (int i = 0; i < n; i++)
 	{
-	    cin>>arr[i];
+		cin >> arr[i];
 	}
-	cout << "Sorted array: \n";
-	printArray(arr, n);
-	return 0;
+	double time_spent = 0.0;
+	auto start = high_resolution_clock::now();
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n - i - 1; j++)
+		{
+			if (arr[j] > arr[j + 1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = temp;
+			}
+		}
+	}
+	auto stop = high_resolution_clock::now();
+	cout << "The sorted array is:\n";
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << '\t';
+	}
+	auto diff = stop - start;
+	// time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
+	cout << '\n'
+		 << setprecision(40) << chrono::duration<double, milli>(diff).count() << " ms" << endl;
 }
-
