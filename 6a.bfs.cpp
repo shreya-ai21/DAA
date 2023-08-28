3e6868cf650da1a839957de34e9bc39cfa0d272f
shreya-ai21 <139116160+shreya-ai21@users.noreply.github.com>
2023-08-28 15:43:49 +0530

    Update 6a.bfs.cpp

--- 6a.bfs.cpp
+++ 6a.bfs.cpp
@@ -5,58 +5,50 @@ int a[10][10], n;
 
 void bfs(int);
 
-int main()
-{
+int main() {
     int i, j, src;
     cout << "Enter the number of nodes: ";
     cin >> n;
+
     cout << "Enter the adjacency matrix:" << endl;
-    for (i = 1; i <= n; i++)
-    {
-        for (j = 1; j <= n; j++)
-        {
+    for (i = 1; i <= n; i++) {
+        for (j = 1; j <= n; j++) {
             cin >> a[i][j];
         }
     }
+
     cout << "Enter the source node: ";
     cin >> src;
+
     bfs(src);
+    
     return 0;
 }
 
-void bfs(int src)
-{
+void bfs(int src) {
     int q[10], f = 0, r = -1, vis[10], i, j;
-    for (j = 1; j <= n; j++)
-    {
+    for (j = 1; j <= n; j++) {
         vis[j] = 0;
     }
     vis[src] = 1;
     r = r + 1;
     q[r] = src;
-    while (f <= r)
-    {
+    while (f <= r) {
         i = q[f];
         f = f + 1;
-        for (j = 1; j <= n; j++)
-        {
-            if (a[i][j] == 1 && vis[j] != 1)
-            {
+        for (j = 1; j <= n; j++) {
+            if (a[i][j] == 1 && vis[j] != 1) {
                 vis[j] = 1;
                 r = r + 1;
                 q[r] = j;
             }
         }
     }
-    for (j = 1; j <= n; j++)
-    {
-        if (vis[j] != 1)
-        {
-            cout << "Node " << j << " is not reachable" << endl;
-        }
-        else
-        {
-            cout << "Node " << j << " is reachable" << endl;
+    for (j = 1; j <= n; j++) {
+        if (vis[j] != 1) {
+            cout << "\nNode " << j << " is not reachable" << endl;
+        } else {
+            cout << "\nNode " << j << " is reachable" << endl;
         }
     }
 }
